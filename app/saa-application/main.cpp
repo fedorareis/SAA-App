b60edf382e93aed1113e685a18878edfb1143c1e
Kyle Piddington <kpiddy@gmail.com>
2015-11-15 14:56:18 -0800

    Added Common tests to each application

--- app/saa-application/main.cpp
+++ app/saa-application/main.cpp
@@ -1,10 +1,14 @@
 /**
  * Entry point of the saa-application
  */
+#include <common/Common.h>
 #include "saa-application/SaaApplication.h"
 int main(int argC, const char* argV[])
 {
    SaaApplication app;
    app.report();
+   Common common;
+   common.report();
+
    return 0;
 }
--- app/test-server/main.cpp
+++ app/test-server/main.cpp
@@ -1,11 +1,15 @@
 /**
  * Entry point of the test-server application
  */
+#include <common/Common.h>
 #include "test-server/TestServer.h"
 int main(int argC, const char* argV[])
 {
    TestServer server;
    server.report();
+   Common common;
+   common.report();
+
 
    return 0;
 }
