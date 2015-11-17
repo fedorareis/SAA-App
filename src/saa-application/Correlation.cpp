5ce6f7f23f064b63d9131a63ea3fbffcb2db4489
fedorareis <fedorareis@gmail.com>
2015-11-17 10:46:51 -0800

    Created Correlation class and header

---
+++ src/saa-application/Correlation.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Kyle Reis on 11/17/15.
+//
+
+#include "Correlation.h"
+
+void Correlation::report()
+{
+   std::cout << "We are correlating some stuff" << std::endl;
+}
---
+++ src/saa-application/Correlation.h
@@ -0,0 +1,12 @@
+//
+// Created by Kyle Reis on 11/17/15.
+//
+
+#ifndef SAA_APPLICATION_CORRELATION_H
+#define SAA_APPLICATION_CORRELATION_H
+class Correlation
+{
+public:
+   void report();
+};
+#endif //SAA_APPLICATION_CORRELATION_H
