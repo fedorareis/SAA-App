a5229f91482e8446ad9df07f566b5d33ff01172a
LeJon McGowan <lejonmcgowan@gmail.com>
2015-11-17 20:29:15 -0800

    NOW it's setup

--- src/cdti/Snapshot.h
+++ src/cdti/Snapshot.h
@@ -38,14 +38,58 @@
 **
 ****************************************************************************/
 
-#include <QApplication>
-#include <cdti/Snapshot.h>
+#ifndef SCREENSHOT_H
+#define SCREENSHOT_H
 
+#include <QPixmap>
+#include <QWidget>
 
-int main(int argc, char *argv[])
+class QCheckBox;
+class QGridLayout;
+class QGroupBox;
+class QHBoxLayout;
+class QLabel;
+class QPushButton;
+class QSpinBox;
+class QVBoxLayout;
+
+class Screenshot : public QWidget
 {
-   QApplication app(argc, argv);
-   Screenshot screenshot;
-   screenshot.show();
-   return app.exec();
-}
+Q_OBJECT
+
+public:
+    Screenshot();
+
+protected:
+    void resizeEvent(QResizeEvent *event);
+
+private slots:
+    void newScreenshot();
+    void saveScreenshot();
+    void shootScreen();
+    void updateCheckBox();
+
+private:
+    void createOptionsGroupBox();
+    void createButtonsLayout();
+    QPushButton *createButton(const QString &text, QWidget *receiver,
+                              const char *member);
+    void updateScreenshotLabel();
+
+    QPixmap originalPixmap;
+
+    QLabel *screenshotLabel;
+    QGroupBox *optionsGroupBox;
+    QSpinBox *delaySpinBox;
+    QLabel *delaySpinBoxLabel;
+    QCheckBox *hideThisWindowCheckBox;
+    QPushButton *newScreenshotButton;
+    QPushButton *saveScreenshotButton;
+    QPushButton *quitScreenshotButton;
+
+    QVBoxLayout *mainLayout;
+    QGridLayout *optionsGroupBoxLayout;
+    QHBoxLayout *buttonsLayout;
+};
+
+#endif
