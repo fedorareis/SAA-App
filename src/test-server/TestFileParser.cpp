bc660efc69fbde1801efd5ea1c26e7e5cef83b97
Hae Ri Hwang <Helen@pcp080283pcs.wireless.calpoly.edu>
2015-11-19 09:58:57 -0800

    comments

--- src/test-server/TestFileParser.cpp
+++ src/test-server/TestFileParser.cpp
@@ -11,6 +11,22 @@ void TestFileParser::load()
 {
    std::cout<<"test file is "<<TestFileParser::testFile<<std::endl;
    // try using the xml parser
+   using namespace rapidxml;
+   xml_document<> doc; // root of the DOM hierarchy; xml_node and a memory_pool
+   //doc.parse<0>(text); // 0 means default parse flags; flags must be a compile-time constant!
+
+   // to access DOM tree, use methods of xml_node and xml_attribute classes
+   /**
+    cout << "Name of my first node is: " << doc.first_node()->name() << "\n";
+    xml_node<> *node = doc.first_node("foobar");
+    cout << "Node foobar has value " << node->value() << "\n";
+    for (xml_attribute<> *attr = node->first_attribute();
+           attr; attr = attr->next_attribute())
+    {
+       cout << "Node foobar has attribute " << attr->name() << " ";
+       cout << "with value " << attr->value() << "\n";
+    }
+    */
 }
 
 // think if I need to add more functions in parsing...
