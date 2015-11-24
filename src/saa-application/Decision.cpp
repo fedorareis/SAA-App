a4d70306ea24f2036d8c865470e5e1ca2783b4ae
fedorareis <fedorareis@gmail.com>
2015-11-24 01:17:11 -0800

    Started work on Plane class

--- src/saa-application/Decision.cpp
+++ src/saa-application/Decision.cpp
@@ -4,16 +4,16 @@
 
 #include <iostream>
 #include <fstream>
-#include <string>
 #include "common/protobuf/cdti.pb.h"
 #include "Decision.h"
+#include "Plane.h"
 
 void Decision::report()
 {
    std::cout << "We are making decisions here" << std::endl;
    CDTIPlane plane;
-   Vector * vector = new Vector();
-   Vector * vector2 = new Vector();
+   Vector *vector = new Vector();
+   Vector *vector2 = new Vector();
    CDTIPlane plane2;
 
    std::string id = "CF34X";
@@ -34,18 +34,32 @@ void Decision::report()
 
    // Write the new plane back to disk.
    std::fstream output("test", std::ios::out | std::ios::trunc | std::ios::binary);
-   if (!plane.SerializeToOstream(&output)) {
+   if (!plane.SerializeToOstream(&output))
+   {
       std::cerr << "Failed to write plane." << std::endl;
    }
 
    output.close();
 
    std::fstream input("test", std::ios::in | std::ios::binary);
-   if (!plane2.ParseFromIstream(&input)) {
+   if (!plane2.ParseFromIstream(&input))
+   {
       std::cerr << "Failed to parse plane." << std::endl;
    }
 
    std::cout << "Plane2 ID: " << plane2.id() << std::endl;
 
    google::protobuf::ShutdownProtobufLibrary();
+
+   Plane plane3 = Plane("X305C", 3, 4, 5, 6, 7, 8);
+   CDTIPlane cdtiPlane = plane3.getCDTIPlane();
+
+   std::cout << "Plane ID: " << cdtiPlane.id() << std::endl;
+   std::cout << "Plane positionX: " << cdtiPlane.position().x() << std::endl;
+   std::cout << "Plane positionY: " << cdtiPlane.position().y() << std::endl;
+   std::cout << "Plane positionZ: " << cdtiPlane.position().z() << std::endl;
+   std::cout << "Plane velocityX: " << cdtiPlane.velocity().x() << std::endl;
+   std::cout << "Plane velocityY: " << cdtiPlane.velocity().y() << std::endl;
+   std::cout << "Plane velocityZ: " << cdtiPlane.velocity().z() << std::endl;
+   std::cout << "Plane severity: " << cdtiPlane.severity() << std::endl;
 }
