c569230ac555860f1d74d6d3e135d2141c0e11fc
Kyle Piddington <kpiddy@gmail.com>
2015-11-23 21:43:21 -0800

    Added sockets that support protocol buffers Added empty headers for some connectionServices, no changes yet

---
+++ src/saa-application/ConnectionServices/AdsBService.h
@@ -0,0 +1,16 @@
+//
+// Created by Kyle Piddington on 11/22/15.
+//
+
+#ifndef SAA_APPLICATION_ADSBSERVICE_H
+#define SAA_APPLICATION_ADSBSERVICE_H
+#include "common/protobuf/adsb.pb.h"
+
+class AdsBService {
+   virtual void init(const ConnectionInfo & info ) = 0;
+   virtual void sendData(AdsBReport report) = 0;
+
+};
+
+
+#endif //SAA_APPLICATION_CDTISERVICE_H
---
+++ src/saa-application/ConnectionServices/RadarService.h
@@ -0,0 +1,8 @@
+//
+// Created by Kyle Piddington on 11/22/15.
+//
+
+#ifndef SAA_APPLICATION_RADARSERVICE_H
+#define SAA_APPLICATION_RADARSERVICE_H
+
+#endif //SAA_APPLICATION_RADARSERVICE_H
