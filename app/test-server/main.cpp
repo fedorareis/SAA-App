e5def7d6bf48ea726c7c3f1298e0f51863b0e31b
Helen Hwang <hhwang02@calpoly.edu>
2015-11-24 10:23:25 -0800

    added new

--- app/test-server/main.cpp
+++ app/test-server/main.cpp
@@ -15,16 +15,22 @@ int main(int argC, const char* argV[])
    common.report();
 
    // Note: find a better way to include resource folder....
-/*
    std::cout<<"\nTest File 1"<<std::endl;
    std::string s("/Users/Helen/Desktop/REPO/saa-application/resources/testCases/TestCaseExample.xml");
    TestFileParser parser(s);
    parser.load();
 
+   // contains multiple planes
    std::cout<<"\nTest File 2"<<std::endl;
    std::string s1("/Users/Helen/Desktop/REPO/saa-application/resources/testCases/TestCaseExample2.xml");
    TestFileParser parser_1(s1);
    parser_1.load();
-   */
+
+   // contains configuration error
+   std::cout<<"\nTest File 3"<<std::endl;
+   std::string s2("/Users/Helen/Desktop/REPO/saa-application/resources/testCases/TestCaseExample3.xml");
+   TestFileParser parser_2(s2);
+   parser_2.load();
+
    return 0;
 }
---
+++ src/test-server/TestCase.h
@@ -0,0 +1,16 @@
+//
+// Created by Hae Ri Hwang on 11/24/15.
+//
+
+#ifndef SAA_APPLICATION_TESTCASE_H
+#define SAA_APPLICATION_TESTCASE_H
+
+
+class TestCase {
+
+public:
+   static TestCase LoadTestCase(std::string fileName);
+};
+
+
+#endif //SAA_APPLICATION_TESTCASE_H
