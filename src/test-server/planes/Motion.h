69a53c39bc29b87b5a83b3e665dc92ff0eed463f
Kyle Piddington <kpiddy@gmail.com>
2015-12-01 19:01:01 -0800

    Really merged

--- src/test-server/planes/Motion.h
+++ src/test-server/planes/Motion.h
@@ -5,25 +5,8 @@
 #ifndef SAA_APPLICATION_MOTION_H
 #define SAA_APPLICATION_MOTION_H
 #define TICKS_PER_SECOND 1 //Linear mapping right now because i'm lazy ~kyle (apoligies)
-<<<<<<< HEAD
-struct Vector3d
-{
-   Vector3d(double x, double y, double z):
-   x(x),
-   y(y),
-   z(z){}
-   Vector3d operator* (float  rhs)
-   {
-      return Vector3d(x*rhs,y*rhs,z*rhs);
-   }
-   double x;
-   double y;
-   double z;
 
-};
-=======
 #include "common/maths.h"
->>>>>>> origin/master
 class Motion {
    public:
    virtual Vector3d getMotionAtTick(float tick) = 0;
