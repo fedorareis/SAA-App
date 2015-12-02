b3fd9b1726fe67184ccc0af9feb983a5f09b38ea
jacrushar <jrussellh96@gmail.com>
2015-12-02 10:41:48 -0800

    small changes

--- src/saa-application/Plane.cpp
+++ src/saa-application/Plane.cpp
@@ -10,8 +10,8 @@ CDTIPlane Plane::getCDTIPlane()
 
    plane.set_id(tailNumber);
    plane.set_severity(severity);
-   plane.set_allocated_position(position);
-   plane.set_allocated_velocity(velocity);
+   plane.set_allocated_position(new Vector(*position));
+   plane.set_allocated_velocity(new Vector(*velocity));
 
    return plane;
 }
