8cee94ff71938df5ac97d24ee9694a61d967de17
fedorareis <fedorareis@gmail.com>
2015-12-01 23:45:37 -0800

    Set up network code for sending data out to the cdti.

--- src/saa-application/SaaApplication.cpp
+++ src/saa-application/SaaApplication.cpp
@@ -7,13 +7,37 @@
 #include "Correlation.h"
 #include "Decision.h"
 
+ServerSocket * SaaApplication::cdtiSocket = nullptr;
+
+void acceptNetworkConnection(ServerSocket *acceptingSocket, ServerSocket *bindingSocket)
+{
+
+   bindingSocket->accept(*acceptingSocket);
+   std::cout << "Server has accepted cdti socket" << std::endl;
+
+}
+
 void SaaApplication::report()
 {
-    std::cout << "Hello from Saa App!" << std::endl;
-    Correlation cor;
-    Decision dec;
-    cor.report();
-    dec.report(&list);
-    auto rep = dec.generateReport(&list);
-    delete rep;
+   setupSockets(4000);
+   std::thread t1(acceptNetworkConnection,&this->cdtiOut, getCdtiSocket());
+   //std::thread t2(acceptNetworkConnection,&this->validationOut, getCdtiSocket());
+   t1.join();
+   //t2.join();
+   std::cout << "Hello from Saa App!" << std::endl;
+   Correlation cor;
+   Decision dec;
+   cor.report();
+   dec.report(&list);
+   auto rep = dec.generateReport(&list);
+   cdtiOut << (*rep);
+   delete rep;
+}
+
+ServerSocket *SaaApplication::getCdtiSocket() {
+   return cdtiSocket;
+}
+
+void SaaApplication::setupSockets(int cdtiPort) {
+   cdtiSocket = new ServerSocket(cdtiPort);
 }
--- src/saa-application/SaaApplication.h
+++ src/saa-application/SaaApplication.h
@@ -7,12 +7,18 @@
 
 #include <vector>
 #include <common/protobuf/cdti.pb.h>
+#include "common/sockets/ServerSocket.h"
 
 class SaaApplication
 {
 public:
    void report();
+   static ServerSocket * getCdtiSocket();
+   static void setupSockets(int CdtiSocket);
 private:
    std::vector<CDTIPlane *> list;
+   static ServerSocket * cdtiSocket;
+   ServerSocket cdtiOut;
+   ServerSocket validationOut;
 };
 #endif //SAA_APPLICATION_SAAAPPLICATION_H
