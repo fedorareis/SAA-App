01f191bd92d974e189b72dfb7434d5d0da05baf7
Helen Hwang <hhwang02@calpoly.edu>
2016-01-20 09:38:59 -0800

    added sensor enum to plane

--- lib/mlpack/src/mlpack/core/boost_backport/README.md
+++
@@ -1,3 +0,0 @@
-The files in this directory are taken from Boost 1.56.0 in order to backport
-serialization support for unordered_map.  Therefore these files are licensed
-under the Boost Software License.
--- test/saa-application/MockCorrelationTest.cpp
+++ test/saa-application/MockCorrelationTest.cpp
@@ -1,11 +1,11 @@
 //
 // Created by Hae Ri Hwang on 1/20/16.
 //
-#include "MockCorrelation.h"
 #include <gtest/gtest.h>
+#include "MockCorrelation.h"
 
 TEST(CorrelationTest, CanCorrelateOne) {
-   MockCorrelation corr;
+  // MockCorrelation corr;
    // make vectors of planes => 2 data adsb and tcas
    // call correlate with it
    // EXPECT_CALL(corr, correlate());
