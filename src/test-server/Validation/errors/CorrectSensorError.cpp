3becf0f54ef186374debd5acfa7fe070f492c35c
Kyle Piddington <kpiddy@gmail.com>
2016-02-10 21:15:26 -0800

    Fixed up sensors, added TestPlan testcases

---
+++ src/test-server/Validation/errors/CorrectSensorError.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Kyle Piddington on 2/4/16.
+//
+
+#include "CorrectSensorError.h"
+
+CorrectSensorError::CorrectSensorError(long timestamp) : TestCaseError(timestamp) {
+
+}
+
+
+std::string CorrectSensorError::description() {
+ return "";
+}
