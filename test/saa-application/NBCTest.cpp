407c5282c04c211bf9eac7c4138ece7546b084aa
Helen Hwang <hhwang02@calpoly.edu>
2016-02-17 17:28:16 -0800

    initial set up for implementing new correlation

---
+++ test/saa-application/NBCTest.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Helen Hwang on 2/17/16.
+//
+#include <gtest/gtest.h>
+#include "saa-application/Correlation.h"
+
+//TODO: implement test
+
+/*
+TEST(TestCorrUsingNBC, basicTest)
+{
+
+}
+*/
