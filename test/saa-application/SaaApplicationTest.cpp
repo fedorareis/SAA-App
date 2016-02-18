d513338101533ee46c746f658de238b462aa5a0a
jacrushar <jrussellh96@gmail.com>
2016-02-17 17:34:22 -0800

    making tests

---
+++ test/saa-application/SaaApplicationTest.cpp
@@ -0,0 +1,15 @@
+//
+// Created by jacrushar on 2/17/16.
+//
+
+#include <gtest/gtest.h>
+#include <common/protobuf/adsb.pb.h>
+#include <common/protobuf/cdti.pb.h>
+
+
+TEST(saa_Test, adsbToRel) {
+   AdsBReport adsb;
+   OwnshipReport ownship;
+
+   // initialize only the fields we need, then call the function, then assert things
+}
