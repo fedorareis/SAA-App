e3bf88c3b3c101f6d8953f33e8bb0ff5f7bac545
Helen Hwang <hhwang02@calpoly.edu>
2016-03-03 08:50:52 -0800

    initial setup for hierarchical clustering

--- src/saa-application/HiercClusteringCorrelation.cpp
+++ src/saa-application/HiercClusteringCorrelation.cpp
@@ -4,10 +4,9 @@
 
 #include <iostream>
 #include <common/sockets/ClientSocket.h>
-#include <mlpack/methods/det/dtree.hpp>
+#include <mlpack/core.hpp>
+#include <mlpack/methods/emst/dtb.hpp>
 #include "HiercClusteringCorrelation.h"
-#include "SensorData.h"
-#include "CorrelatedData.h"
 
 /**
  * Implements single-linkage hierarchical clustering using euclidean minimum spanning tree (EMST)
@@ -15,89 +14,34 @@
 std::vector<CorrelatedData> HiercClusteringCorrelation::correlate(std::vector<SensorData> planes)
 {
    std::vector<CorrelatedData> correlatedPlanes;
-   std::vector<double> positions_x, positions_y, positions_z, velocities_x, velocities_y, velocities_z;
-   int i, j, k, l, m, n, ndx, innerNdx;
-   int size = planes.size(), originalSize;
+   int size = planes.size();
 
-
-   // euclidean distance = just between 2 points, dimention doesn't matter supposedly
-
-   /*
-    // general use of tree
-    extern arma::mat data; // We want to find the MST of this dataset.
-    DualTreeBoruvka<> dtb(data); // Create the tree with default options.
-    // Find the MST.
-    arma::mat mstResults;
-    dtb.ComputeMST(mstResults);
-    */
-
-
-   // The number of clusters we are getting.
-   size_t clusters = 2; //potentially empty clusters.
-   // The assignments will be stored in this vector.
-  // arma::Col<size_t> groupings;
-   // The centroids will be stored in this matrix.
-  // arma::mat centroids;
-   // Initialize with the default arguments.
-   //KMeans<> km;
-
-   // Add individual sensor info (positions and velocities) into respective lists
-   for (i = 0; i < size; i++)
-   {
-      positions_x.push_back(planes.at(i).getPurePosition().x);
-   }
-   for (j = 0; j < size; j++)
-   {
-      positions_y.push_back(planes.at(j).getPurePosition().y);
-   }
-   for (k = 0; k < size; k++)
-   {
-      positions_z.push_back(planes.at(k).getPurePosition().z);
-   }
-   for (l = 0; l < size; l++)
-   {
-      velocities_x.push_back(planes.at(l).getVelocity().x);
-   }
-   for (m = 0; m < size; m++)
-   {
-      velocities_y.push_back(planes.at(m).getVelocity().y);
-   }
-   for (n = 0; n < size; n++)
-   {
-      velocities_z.push_back(planes.at(n).getVelocity().z);
-   }
-
-   // Array to matrix conversion
-   //Need to be column major
    std::vector<double> data;
-   data.reserve(positions_x.size() + positions_y.size() + positions_z.size());
-   for(int i = 0; i < size; i++)
+   if(planes.size() == 0)
    {
-      data.push_back(positions_x[i]);
-      data.push_back(positions_y[i]);
-      data.push_back(positions_z[i]);
+      return correlatedPlanes;
    }
 
-   /*
-   auto dataMtx = arma::Mat<double>(&data[0],3,planes.size());
+   // put data into matrix for clustering algorithm to use
+   for(int i = 0 ; i < planes.size(); i++){
+      SensorData plane = planes[i];
 
-
-   km.Cluster(dataMtx, clusters, groupings,centroids);
-
-   //@TODO Correlated size should be different
-   correlatedPlanes.reserve(clusters);
-   for(int i = 0; i < clusters; i++)
-   {
-      correlatedPlanes.push_back(CorrelatedData(centroids.at(0,i),centroids.at(1,i),centroids.at(2,i),0,0,0));
-   }
-   for(int i = 0; i < size; i++)
-   {
-      correlatedPlanes[groupings.at(i)].addSensor(planes[i].getSensor(),planes[i].getPlaneTag());
+      // only getting position for now
+      data.push_back(plane.getPurePosition().x);
+      data.push_back(plane.getPurePosition().y);
+      data.push_back(plane.getPurePosition().z);
    }
 
+   auto dataMtx = arma::Mat<double>(&data[0],3,size);
 
+   // default option, might have to add bool for native
+   mlpack::emst::DualTreeBoruvka<> dtb(dataMtx);
+   arma::mat mstResults;
 
-   }
-*/
+   dtb.ComputeMST(mstResults);
+
+   //TODO: NOT IMPLEMENTED YET
+   // traverse the mstResults for stuff...LOL
+
    return correlatedPlanes;
 }
