77cf05961cd6e22039a1049d21ddb02de980bf3b
Helen Hwang <hhwang02@calpoly.edu>
2016-03-06 23:02:52 -0800

    added suggestions and steps for clustering

--- src/saa-application/HiercClusteringCorrelation.cpp
+++ src/saa-application/HiercClusteringCorrelation.cpp
@@ -8,6 +8,7 @@
 #include <mlpack/methods/emst/dtb.hpp>
 #include "HiercClusteringCorrelation.h"
 
+
 /**
  * Implements single-linkage hierarchical clustering using euclidean minimum spanning tree (EMST)
  */
@@ -41,7 +42,52 @@ std::vector<CorrelatedData> HiercClusteringCorrelation::correlate(std::vector<Se
    dtb.ComputeMST(mstResults);
 
    //TODO: NOT IMPLEMENTED YET
-   // traverse the mstResults for stuff...LOL
+
+   // prints the matrix
+   std::cout<<"hc-result matrix contains "<< mstResults.n_rows<<" row elements"<<std::endl;
+   std::cout<<"hc-result matrix contains "<< mstResults.n_cols<<" col elements"<<std::endl;
+   std::cout<<"<index    >index      distance"<<std::endl;
+   std::cout<<mstResults.t()<<std::endl;
+   // indices refers back to the vector that was used to created
+   // and the matrix is ordered by the minimum distance
+   /* A single-linkage clustering can be obtained from the EMST by deleting all edges longer than a given cluster length. */
+   std::cout<<"hc-result matrix has max "<< mstResults.max()<<" and min "<< mstResults.min()<<std::endl;
+   std::cout<<"hc-result matrix index 0 has connection to: " <<std::endl;
+
+   /* Helen's observation
+    *
+    * Few useful functions
+    * Printing .t() prints the values in table format
+    * .n_rows actually gives the number of columns which is 3 all the time
+    * .n_cols gives the number of rows, which I think is size-1?
+    * min() returns the minimum distance, max() returns the maximum distance
+    * So calling this computeMST() returns the "iteration"
+    *
+    * so the matrix is ordered by the minimum distance
+    * First column is the lesser of the index, second column is the greater index, and the third column is the distance
+    *
+
+   // This is the "steps" for single linked hierarchical clustering
+   // This is computed by calling computeMST()
+   Step 1. Construct the distance matrix from the given pattern matrix
+
+    // there's a cor(x, y) function in arma
+    // as long as it's in same dimension, arma can correlate vector or matrix.
+   Step 2. Assign each pattern to a cluster
+
+    // I think you can merge by calling cor(x, y) with vector returns the correlated vector
+    // that contains the dot product
+   Step 3. Determine the smallest entry in the distance matrix D, say D(c1, c2) and merge the
+            two clusters c1 and c2
+
+   Step 4. Update the distance matrix D, by deleting the row and column corresponding to
+             the cluster c2, and rename row c1 and column c1 to (c1, c2).
+            Assign the min[D(c3, c1), D(c3, c2)] to D(c3, (c1, c2)) and D((c1, c2), c3) for all c3’s
+
+   Step 5. If only one cluster is left, stop. Else, go to Step 3
+
+    * */
 
    return correlatedPlanes;
 }
+
