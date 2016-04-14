d34df267de7208962570f7c620903e34619ec1bc
lejonmcgowan <lejonmcgowan@gmail.com>
2016-04-14 02:07:08 -0700

    redid lost script parts. thanks vim

--- build.sh
+++ build.sh
@@ -6,7 +6,17 @@ echo "ML pack dependicies"
 sudo apt-get install libarmadillo-dev libboost-all-dev qtdeclarative5-dev
 echo "Now to finish up builing mlpack"
 cd lib/mlpack
+if [ -d "build" ]; then
+	rm -rf build/
+fi
 mkdir build
 cd build
 cmake ..
-
+make
+cd ../../../
+if[-d "build" ]; then
+	rm -rf build/
+fi
+mkdir build
+cd build
+cmake ..
