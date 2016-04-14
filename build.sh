af81a513a75bd0066359f43fb0871e130bcfe6ae
lejonmcgowan <lejonmcgowan@gmail.com>
2016-04-13 21:38:18 -0700

    minor change to take into account created directory

--- build.sh
+++ build.sh
@@ -3,12 +3,9 @@ git submodule init
 echo "Updating submodules"
 git submodule update
 echo "ML pack dependicies"
-sudo apt-get install lib-armidillo-dev libboost-all-dev
+sudo apt-get install libarmidillo-dev libboost-all-dev qt5declarative-dev
 echo "Now to finish up builing mlpack"
 cd lib/mlpack
-if [ -d "$DIRECTORY" ]; then
-	rm -rf build
-fi
 mkdir build
 cd build
 cmake ..
