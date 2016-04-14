bdf8f5ed66d3e82f25d1dc885913e85974b1e81a
lejonmcgowan <lejonmcgowan@gmail.com>
2016-04-13 21:44:16 -0700

    minor change to take into account created directory

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ git submodule init
 echo "Updating submodules"
 git submodule update
 echo "ML pack dependicies"
-sudo apt-get install libarmidillo-dev libboost-all-dev qt5declarative-dev
+sudo apt-get install libarmadillo-dev libboost-all-dev qtdeclarative5-dev
 echo "Now to finish up builing mlpack"
 cd lib/mlpack
 mkdir build
