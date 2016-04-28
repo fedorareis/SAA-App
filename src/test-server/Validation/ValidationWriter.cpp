d1d0c578c96386d960fe820a4d4b19d469f7d57a
jacrushar <jrussellh96@gmail.com>
2016-04-27 23:38:45 -0700

    adding csv output functionality for test results

--- src/test-server/Validation/ValidationWriter.cpp
+++ src/test-server/Validation/ValidationWriter.cpp
@@ -4,8 +4,14 @@
 
 #include "ValidationWriter.h"
 #include "Validator.h"
+#include <iostream>
+#include <fstream>
 
 void ValidationWriter::writeErrors(std::ostream &str, const Validator &v) {
+   std::ofstream out;
+   out.open("errors.csv");
+   out << "Time Stamp,Expected,Actual\n";
+
    if(v.hasErrors())
    {
       if(!v.hasReceivedResults())
@@ -16,6 +22,7 @@ void ValidationWriter::writeErrors(std::ostream &str, const Validator &v) {
          for (auto error : v.getErrors()) {
             str << "Correlation error: [" << error->getTimestamp() << "] : "
             << error->description() << std::endl;
+            out << error->getTimestamp() << "," << "\n";
          }
       }
    }
@@ -23,4 +30,5 @@ void ValidationWriter::writeErrors(std::ostream &str, const Validator &v) {
    {
       str << "Correlation is perfect, no errors detected!" << std::endl;
    }
+   out.close();
 }
