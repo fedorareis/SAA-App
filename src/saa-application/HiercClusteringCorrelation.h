0f46bb7ab5a9dba1cf810bc73ec175eb146ccfc3
Leah <lpichay@calpoly.edu>
2016-05-05 17:33:39 -0700

    Hierarchical header fixed

--- src/saa-application/HiercClusteringCorrelation.h
+++ src/saa-application/HiercClusteringCorrelation.h
@@ -12,8 +12,6 @@
 class HiercClusteringCorrelation : public CorrelationStrategy
 {
 public:
-   void report();
-   //void addPlane(/* When we have a plane class pass one in here*/);
    virtual std::vector<CorrelatedData> correlate(std::vector<SensorData> planes);
 };
 #endif //SAA_APPLICATION_HIERCCLUSTERINGCORRELATION_H
