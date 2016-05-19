48180fbf912bf8af19a1168bcfa687b2ecabc6d7
Helen Hwang <hhwang02@calpoly.edu>
2016-05-19 08:26:19 -0700

    welc on hierchical

--- src/saa-application/HiercClusteringCorrelation.h
+++ src/saa-application/HiercClusteringCorrelation.h
@@ -8,12 +8,15 @@
 #include "SensorData.h"
 #include "CorrelatedData.h"
 #include "CorrelationStrategy.h"
+#include "ClusteringTechnique.h"
 
 class HiercClusteringCorrelation : public CorrelationStrategy
 {
 public:
-   void report();
    //void addPlane(/* When we have a plane class pass one in here*/);
    virtual std::vector<CorrelatedData> correlate(std::vector<SensorData> planes);
+
+private:
+   ClusteringTechnique& cluster;
 };
 #endif //SAA_APPLICATION_HIERCCLUSTERINGCORRELATION_H
